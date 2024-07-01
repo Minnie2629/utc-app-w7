b719f4d42cf570c65685835c23393bcfc829ade9
Minnie2629 <miniath100k@yahoo.fr>
2024-07-01 01:49:10 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "server1" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public1.id
   security_groups = aws_security_group.sg-demo.id
+vpc_security_group_ids = [aws_security_group.sg-demo.id]
   key_name      = aws_key_pair.aws_key.key_name
   user_data     = file("server.sh")
   tags ={
